12a0d6e867dc5e426df730d9b6b5fed2b26be6f9
eldainjjj <eldainjjj@gmail.com>
2026-06-08 22:09:51 +0700

    update

--- script.js
+++ script.js
@@ -1,14 +1,61 @@
-let btn = document.getElementById("add-subject")
-let pop = document.getElementById("win-pop")
-let createbtn = document.createElement("button")
-let open = false
+let btn = document.getElementById("add-subject");
+let pop = document.getElementById("win-pop");
+let tracker = document.getElementById("tracker-header");
+const uniqueId = `item-${crypto.randomUUID()}`;
+let submit = document.getElementById("submit");
+let subjectinput = document.getElementById("subject-name");
+let open = false;
 
 btn.addEventListener("click", () => {
+    open = !open;
+
     if (open) {
-        pop.style.display = "none";
-        open = false;
-    } else {
+        pop.style.opacity = "1";
+        pop.style.transform = "translateY(0px)";
+        pop.style.visibility = "visible";
         pop.style.display = "flex";
-        open = true;
+    } else {
+        pop.style.opacity = "0";
+        pop.style.transform = "translateY(-5px)";
+        pop.style.visibility = "hidden";
+        pop.style.display = "none";
     }
-})
+});
+submit.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    let subject = subjectinput.value;
+
+    let div = document.createElement("div");
+    let del = document.createElement("button");
+    let header = document.createElement("h3");
+
+    header.textContent = subject;
+    del.textContent = "Del";
+
+    div.style.width = "500px";
+    div.style.height = "70px";
+    div.style.border = "1px solid black";
+    div.style.background = "rgba(115, 125, 125,0.2)";
+    div.style.margin = "5px auto";
+    div.style.borderRadius = "5px";
+
+    div.style.display = "flex";
+    div.style.justifyContent = "space-between";
+    div.style.alignItems = "center";
+
+    header.style.margin = "0";
+
+    del.style.width = "40px";
+    del.style.height = "40px";
+    del.style.background = "rgb(58, 58, 144)";
+    del.style.color = "white";
+
+    del.addEventListener("click", () => {
+        div.remove();
+    });
+
+    div.appendChild(header);
+    div.appendChild(del);
+    tracker.appendChild(div);
+});
